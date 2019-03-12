dffa19f076f29bae6966030e399f69ab14c37002
Viraat Das <viraat.laldas@gmail.com>
2019-03-12 18:52:31 -0400

    First commit.

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+inputNum = input("Enter an integer: \n")
+
+#Check if enter value is proper
+try:
+    savedNum = int(inputNum)
+except ValueError:
+    print("That's not proper!")
+    quit()
+    sys.exit()
+
+if savedNum == 2:
+    print("That's 2!")
+elif savedNum == 5:
+    print("That's 5, you silly!")
+elif savedNum % 2 == 1:
+    print("Odd")
+else:
+    print("Even")
